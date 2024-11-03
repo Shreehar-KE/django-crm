84fd501ac71cdfdaaa19990ae21eabe98a977ad6
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-11-03 12:39:04 +0530

    before app rename commit

--- a_customers/views.py
+++ a_customers/views.py
@@ -6,5 +6,6 @@ from .models import Contact
 class HomePageView(ListView):
     model = Contact
     context_object_name = "contacts"
+    ordering = "-date_time_added"
     template_name = 'a_customers/table.html'
 
