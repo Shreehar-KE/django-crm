99100a07ef2df0305415be46f09cf41e2f7f9e69
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-11-13 02:07:58 +0530

    add table filter

--- a_contacts/views.py
+++ a_contacts/views.py
@@ -1,7 +1,7 @@
 import random
 import time
 import urllib
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import (
     ListView, CreateView, UpdateView, DetailView)
@@ -10,21 +10,30 @@ from .models import Contact
 from .forms import ContactForm
 from faker import Faker
 
-
 class HomePageView(ListView):
     model = Contact
     search_text = ''
+    selected_filters = []
     is_result = True
+    is_search_text = False
     context_object_name = "contacts"
-    ordering = "contact_id"
     template_name = 'a_contacts/home.html'
-    paginate_by = 10
+    paginate_by = 20
+
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        self.selected_filters = request.GET.getlist('type')
+        self.search_text = self.request.GET.get("search_text", "")
+        if self.search_text:
+            self.search_text = urllib.parse.unquote(self.search_text)
+            self.search_text = self.search_text.strip()
+            self.is_search_text = True
+            print('search: ',self.is_search_text)
+        return super().get(request, *args, **kwargs)
 
     def render_to_response(self, context, **response_kwargs):
         response_kwargs.setdefault("content_type", self.content_type)
 
         if self.request.headers.get('Hx-Request'):
-            time.sleep(1)
             return self.response_class(
                 request=self.request,
                 template="partials/table_row.html",
@@ -42,22 +51,30 @@ class HomePageView(ListView):
             )
 
     def get_queryset(self):
-        self.search_text = self.request.GET.get("search_text", "")
-        self.search_text = urllib.parse.unquote(self.search_text)
-        self.search_text = self.search_text.strip()
         search_text_length = len(self.search_text)
 
+        if self.selected_filters:
+            contacts = Contact.objects.filter(type__in=self.selected_filters)
+            
+        else:
+            contacts = super().get_queryset()
+            
         if self.search_text and search_text_length > 3:
             if self.search_text.startswith('#'):
                 q = self.search_text[1:].lstrip('0')
-                try:
-                    id_length = len(q)
 
-                    if (id_length in (1, 2) and search_text_length == 4) or (id_length >= 3 and search_text_length == id_length+1):
-                        queryset = Contact.objects.filter(contact_id=q)
-                except:
+                if q.isdigit():
+                    id_length = len(q)
+                else:
                     self.is_result = False
-            else: 
+                    return Contact.objects.none()
+                if (id_length in (1, 2) and search_text_length == 4) or (id_length >= 3 and search_text_length == id_length+1):
+                    queryset = contacts.filter(contact_id=q)
+                else:
+                    self.is_result = False
+                    return Contact.objects.none()
+
+            else:
                 parts = self.search_text.split()
 
                 q = Q(first_name__icontains=parts[0]) | Q(last_name__icontains=parts[0]) | Q(
@@ -65,22 +82,31 @@ class HomePageView(ListView):
                 for part in parts[1:]:
                     q |= Q(first_name__icontains=part) | Q(last_name__icontains=part) | Q(
                         email__icontains=part) | Q(location__icontains=part) | Q(type__icontains=part)
-                queryset = Contact.objects.filter(q)
-                
+                queryset = contacts.filter(q)
+
             if queryset:
                 return queryset
             else:
                 self.is_result = False
+                return Contact.objects.none()
         else:
-            return super().get_queryset()
+            return contacts
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        page_obj = context['page_obj']
-        context['page_offset'] = (page_obj.number - 1) * self.paginate_by
-        context['more_contacts'] = page_obj.has_next()
-        if page_obj.has_next():
-            context['next_page'] = page_obj.next_page_number()
+        context['is_result'] = self.is_result
+        context['is_search_text'] = self.is_search_text
+        
+        if self.is_result and context.get('page_obj'):
+            page_obj = context['page_obj']
+            context['page_offset'] = (page_obj.number - 1) * self.paginate_by
+            context['more_contacts'] = page_obj.has_next()
+            context['next_page'] = page_obj.next_page_number() if page_obj.has_next() else None
+        else:
+            context['page_offset'] = 0
+            context['more_contacts'] = False
+            context['next_page'] = None
+            context['contacts'] = []
 
         context['lead_count'] = Contact.objects.filter(type='LEAD').count()
         context['prospect_count'] = Contact.objects.filter(
@@ -88,10 +114,9 @@ class HomePageView(ListView):
         context['customer_count'] = Contact.objects.filter(
             type='CUSTOMER').count()
         context['search_text'] = self.search_text
-        context['is_result'] = self.is_result
+        context['selected_filters'] = self.selected_filters
 
         return context
-
 
 class ContactCreateView(CreateView):
     form_class = ContactForm
