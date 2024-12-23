7fc21b511a0a219d0c23ca090bc6ce55b44e42a8
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-12-23 09:53:40 +0530

    update approval required middleware

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import redirect
 from django.urls import reverse
 
@@ -7,7 +8,7 @@ class ApprovalRequiredMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated:
+        if request.user.is_authenticated and request.user.username != settings.ADMIN_NAME:
             if not request.user.is_approved and request.path not in [
                 reverse("accounts:approval_status"),
                 reverse("analytics:live-users"),
